aad32b747f01681772887698fb1eaaf9dc82d8fe
greemaltend4 <dengestabiy@hotmail.com>
2025-10-31 09:05:16 +0000

    Update welcome message to include API version.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Bridge Cloud Integration Service! Your gateway to seamless integration.');
+  res.send('Welcome to the Bridge Cloud Integration Service! Your gateway to seamless integration. API version 1.0');
 });
 
 app.listen(port, () => {
